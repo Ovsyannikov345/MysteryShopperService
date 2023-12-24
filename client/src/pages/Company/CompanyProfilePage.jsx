42b64c6226d420317810f81a6dc91da4cfddcdd2
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-12-24 23:29:38 +0300

    feat: add header to company profile page

--- client/src/pages/Company/CompanyProfilePage.jsx
+++ client/src/pages/Company/CompanyProfilePage.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Grid } from "@mui/material";
+import CompanyHeader from './../../components/headers/CompanyHeader';
 
 const CompanyProfilePage = () => {
     return (
-        <>
-            <h1>This is company profile with jwt {localStorage.getItem("jwt")}</h1>
-            <Button
-                onClick={() => {
-                    localStorage.removeItem("jwt");
-                    localStorage.removeItem("role");
-                    window.location.reload();
-                }}
+        <Grid
+            container
+            width={"100%"}
+            height={"100vh"}
+            flexDirection={"column"}
+            justifyContent={"flex-start"}
+            alignItems={"center"}
+            bgcolor={"#E7E7E7"}
+        >
+            <CompanyHeader />
+            <Grid
+                container
+                item
+                flexDirection={"column"}
+                alignItems={"center"}
+                maxWidth={"1300px"}
+                flexGrow={1}
+                bgcolor={"#FFFFFF"}
             >
-                CLEAR
-            </Button>
-        </>
+                <h1>This is company profile page</h1>
+            </Grid>
+        </Grid>
     );
 };
 
