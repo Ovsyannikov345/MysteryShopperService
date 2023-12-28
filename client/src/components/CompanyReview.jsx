87ecf34bde8fd766531e3635ad256e0173f058db
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-12-28 23:05:35 +0300

    feat: add user profile link to company review

--- client/src/components/CompanyReview.jsx
+++ client/src/components/CompanyReview.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Grid, Typography, Rating, Avatar, Stack } from "@mui/material";
+import { Grid, Typography, Rating, Avatar, Stack, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const CompanyReview = ({ companyReview }) => {
+    const navigate = useNavigate();
+
     return (
         <Grid
             container
@@ -10,17 +13,19 @@ const CompanyReview = ({ companyReview }) => {
             gap={"8px"}
             style={{ border: "2px solid #DDC12C", borderRadius: "10px" }}
         >
-            <Avatar
-                src={
-                    companyReview.User.id !== undefined
-                        ? `http://localhost:5000/api/users/${
-                              companyReview.User.id
-                          }/avatar?jwt=${localStorage.getItem("jwt")}`
-                        : ""
-                }
-                variant="square"
-                sx={{ width: 60, height: 60 }}
-            />
+            <Button style={{padding: 0}} onClick={() => navigate(`/user/${companyReview.User.id}`)}>
+                <Avatar
+                    src={
+                        companyReview.User.id !== undefined
+                            ? `http://localhost:5000/api/users/${
+                                  companyReview.User.id
+                              }/avatar?jwt=${localStorage.getItem("jwt")}`
+                            : ""
+                    }
+                    variant="square"
+                    sx={{ width: 60, height: 60 }}
+                />
+            </Button>
             <Stack direction={"row"} justifyContent={"space-between"} flexGrow={1}>
                 <Typography variant="h2" height={"35px"} style={{ borderBottom: "2px solid #DDC12C" }}>
                     {companyReview.User.surname + " " + companyReview.User.name}
