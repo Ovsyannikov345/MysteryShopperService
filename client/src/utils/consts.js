4fc95ba29f1775171bbac0be7c04a885cc55926a
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-01-03 01:40:52 +0300

    refactor: remove redundant paths

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,5 +1,3 @@
 export const LOGIN_ROUTE = "/login";
-export const COMPANY_REGISTER_ROUTE = "/register/company";
-export const USER_REGISTER_ROUTE = "/register/user";
 export const USER_PROFILE_ROUTE = "/user";
 export const COMPANY_PROFILE_ROUTE = "/company";
