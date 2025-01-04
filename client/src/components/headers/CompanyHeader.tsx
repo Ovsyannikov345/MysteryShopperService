76053abfaa862e5bcb9ce13fef823fe2cec72a96
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2025-01-04 21:10:13 +0300

    Change headers

--- client/src/components/headers/CompanyHeader.tsx
+++ client/src/components/headers/CompanyHeader.tsx
@@ -1,79 +1,185 @@
-import React from "react";
-import { Grid, Stack, Typography, SpeedDial, SpeedDialAction, useMediaQuery, Theme } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import {
+    Grid2 as Grid,
+    useMediaQuery,
+    Button,
+    Menu,
+    MenuItem,
+    ListItemIcon,
+    AppBar,
+    Toolbar,
+    Box,
+    Tooltip,
+    IconButton,
+    Avatar,
+    Paper,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
-import logo from "../../images/logo-cropped.png";
-import HeaderIcons from "./HeaderIcons";
+import logo from "../../images/logo-cropped2.png";
+import SupportAgentRoundedIcon from "@mui/icons-material/SupportAgentRounded";
+import ProfileIcon from "@mui/icons-material/Person";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import LogoutIcon from "@mui/icons-material/Logout";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import AddIcon from "@mui/icons-material/Add";
 import MenuIcon from "@mui/icons-material/Menu";
-import AddBoxIcon from "@mui/icons-material/AddBox";
-import AssignmentIcon from "@mui/icons-material/Assignment";
+import useAuthApi from "../../hooks/useAuthApi";
 
 const CompanyHeader = () => {
-    const isScreenSizeUpMd = useMediaQuery((theme: Theme) => theme.breakpoints.up("md"));
-
     const theme = useTheme();
 
-    const actions = [
-        { icon: <AddBoxIcon />, name: "Создать заказ", link: "/orders/create" },
-        { icon: <AssignmentIcon />, name: "Мои заказы", link: "/my-orders" },
-    ];
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+    const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
 
     const navigate = useNavigate();
 
+    const [profileMenuAnchorEl, setProfileMenuAnchorEl] = useState<Element | null>(null);
+
+    const [actionsMenuAnchorEl, setActionsMenuAnchorEl] = useState<Element | null>(null);
+
+    const { logout } = useAuthApi();
+
+    const onLogout = async () => {
+        await logout();
+    };
+
+    const onProfileMenuClick = (destinationRoute: string) => {
+        setProfileMenuAnchorEl(null);
+        navigate(destinationRoute);
+    };
+
+    const onActionsMenuClick = (destinationRoute: string) => {
+        setActionsMenuAnchorEl(null);
+        navigate(destinationRoute);
+    };
+
     return (
-        <Grid
-            container
-            width={"100%"}
-            bgcolor={theme.palette.primary.main}
-            pl={"42px"}
-            alignItems={"center"}
-            sx={{ height: { xs: "85px", md: "160px" }, justifyContent: { xs: "flex-end", md: "space-between" } }}
-        >
-            {isScreenSizeUpMd ? (
-                <Stack direction={"row"} sx={{ gap: { xs: "30px", lg: "93px" } }}>
-                    <img src={logo} alt="logo" height={"100px"} />
-                    <Typography variant="h1" textAlign={"center"}>
-                        <Grid container item height={"100%"} alignItems={"center"}>
-                            <Link to={"/orders/create"} style={{ textDecoration: "none", color: "#000000" }}>
-                                <Typography variant="h1" sx={{ fontSize: { xs: "24px", lg: "32px" } }}>
-                                    Создать заказ
-                                </Typography>
-                            </Link>
+        <>
+            <Box id="header">
+                <AppBar position="static">
+                    <Toolbar style={{ justifyContent: "space-between" }}>
+                        <Grid container mt={"5px"} mb={"5px"}>
+                            <Grid container gap={"60px"} alignItems={"center"}>
+                                <IconButton
+                                    edge="start"
+                                    color="inherit"
+                                    aria-label="menu"
+                                    onClick={(event) => {
+                                        setActionsMenuAnchorEl(event.currentTarget);
+                                    }}
+                                    sx={{ display: isSmallScreen ? "flex" : "none" }}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <img
+                                    src={logo}
+                                    alt="Logo"
+                                    style={{
+                                        maxWidth: isSmallScreen ? "130px" : "200px",
+                                        height: "auto",
+                                        cursor: "pointer",
+                                        borderRadius: "10px",
+                                        display: isSmallScreen ? "none" : "flex",
+                                    }}
+                                    onClick={() => navigate("/")}
+                                />
+                                <Grid container spacing={5} sx={{ display: isMediumScreen ? "none" : "flex" }}>
+                                    <Paper elevation={6} sx={{ borderRadius: "10px", bgcolor: theme.palette.primary.light }}>
+                                        <Button
+                                            variant="text"
+                                            color="secondary"
+                                            style={{ fontSize: "18px", padding: "10px", borderRadius: "10px" }}
+                                            startIcon={<AddIcon />}
+                                            sx={{ textTransform: "capitalize" }}
+                                        >
+                                            Create order
+                                        </Button>
+                                    </Paper>
+                                    <Paper elevation={6} sx={{ borderRadius: "10px", bgcolor: theme.palette.primary.light }}>
+                                        <Button
+                                            variant="text"
+                                            color="secondary"
+                                            style={{ fontSize: "18px", padding: "10px", borderRadius: "10px" }}
+                                            startIcon={<FormatListBulletedIcon />}
+                                            sx={{ textTransform: "capitalize" }}
+                                        >
+                                            My orders
+                                        </Button>
+                                    </Paper>
+                                </Grid>
+                            </Grid>
                         </Grid>
-                    </Typography>
-                </Stack>
-            ) : (
-                <SpeedDial
-                    ariaLabel="SpeedDial basic example"
-                    sx={{ position: "absolute", top: 10, left: 16 }}
-                    icon={<MenuIcon />}
-                    direction="down"
-                >
-                    {actions.map((action) => (
-                        <SpeedDialAction
-                            key={action.name}
-                            icon={action.icon}
-                            tooltipTitle={action.name}
-                            onClick={() => navigate(action.link)}
-                        />
-                    ))}
-                </SpeedDial>
-            )}
-            <Stack direction={"row"} sx={{ marginRight: { xs: "10px", lg: "50px" }, gap: { xs: "20px", lg: "50px" } }}>
-                {isScreenSizeUpMd && (
-                    <Typography variant="h1" textAlign={"center"}>
-                        <Grid container item height={"100%"} alignItems={"center"}>
-                            <Link to={"/my-orders"} style={{ textDecoration: "none", color: "#000000" }}>
-                                <Typography variant="h1" sx={{ fontSize: { xs: "24px", lg: "32px" } }}>
-                                    Мои заказы
-                                </Typography>
-                            </Link>
+                        <Grid container>
+                            <Grid container gap={isSmallScreen ? "0px" : "10px"} alignItems={"center"}>
+                                <Tooltip title="Notifications">
+                                    <IconButton color="secondary">
+                                        <NotificationsRoundedIcon fontSize={isSmallScreen ? "medium" : "large"} />
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Support">
+                                    <IconButton color="secondary">
+                                        <SupportAgentRoundedIcon fontSize={isSmallScreen ? "medium" : "large"} />
+                                    </IconButton>
+                                </Tooltip>
+                                <Tooltip title="Account">
+                                    <IconButton
+                                        onClick={(event) => {
+                                            setProfileMenuAnchorEl(event.currentTarget);
+                                        }}
+                                    >
+                                        <Avatar sx={{ width: { xs: 30, sm: 50 }, height: { xs: 30, sm: 50 } }} />
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
                         </Grid>
-                    </Typography>
-                )}
-                <HeaderIcons />
-            </Stack>
-        </Grid>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <Menu
+                id="menu"
+                anchorEl={profileMenuAnchorEl}
+                open={Boolean(profileMenuAnchorEl)}
+                onClose={() => setProfileMenuAnchorEl(null)}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+                <MenuItem key={1}>
+                    <ListItemIcon sx={{ mr: "5px" }}>
+                        <ProfileIcon fontSize="small" />
+                    </ListItemIcon>
+                    My profile
+                </MenuItem>
+                <MenuItem key={2} sx={{ mr: "5px" }} onClick={onLogout}>
+                    <ListItemIcon>
+                        <LogoutIcon fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+            <Menu
+                id="menu"
+                anchorEl={actionsMenuAnchorEl}
+                open={Boolean(actionsMenuAnchorEl)}
+                onClose={() => setActionsMenuAnchorEl(null)}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            >
+                <MenuItem key={3}>
+                    <ListItemIcon sx={{ mr: "5px" }}>
+                        <AddIcon fontSize="small" />
+                    </ListItemIcon>
+                    Create Order
+                </MenuItem>
+                <MenuItem key={4}>
+                    <ListItemIcon sx={{ mr: "5px" }}>
+                        <FormatListBulletedIcon fontSize="small" />
+                    </ListItemIcon>
+                    My Orders
+                </MenuItem>
+            </Menu>
+        </>
     );
 };
 
