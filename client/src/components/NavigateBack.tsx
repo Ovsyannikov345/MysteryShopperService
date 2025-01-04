3495c414e95baca852a324e15afe1bbca8cf5cb0
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2025-01-04 21:09:22 +0300

    Change navigate back button

--- client/src/components/NavigateBack.tsx
+++ client/src/components/NavigateBack.tsx
@@ -1,7 +1,7 @@
-import { Grid, IconButton, Theme, Typography, useMediaQuery } from "@mui/material";
+import { Button } from "@mui/material";
 import React from "react";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
+import ArrowBack from "@mui/icons-material/ArrowBack";
 
 interface NavigateBackProps {
     to: any;
@@ -11,15 +11,10 @@ interface NavigateBackProps {
 const NavigateBack = ({ to, label }: NavigateBackProps) => {
     const navigate = useNavigate();
 
-    const isScreenSizeUpMd = useMediaQuery((theme: Theme) => theme.breakpoints.up("md"));
-
     return (
-        <Grid container item height={"60px"} alignItems={"center"} gap={"15px"} flexWrap={"nowrap"}>
-            <IconButton style={{ padding: 0, color: "#000000" }} onClick={() => navigate(to)}>
-                <ArrowBackIcon sx={{ fontSize: { xs: 30, md: 50 } }} />
-            </IconButton>
-            <Typography variant="h2">{isScreenSizeUpMd ? label : ""}</Typography>
-        </Grid>
+        <Button variant="contained" startIcon={<ArrowBack />} onClick={() => navigate(to)}>
+            {label}
+        </Button>
     );
 };
 
