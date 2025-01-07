a3ea295ab2ecd993eeeaf4a633a6eed6bbe17d6a
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2025-01-07 04:48:32 +0300

    Add company image controller

---
+++ server/MysteryShopper.API/ImageControllers/CompanyImageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MysteryShopper.API.Extensions;
+using MysteryShopper.BLL.ImageServices.IImageServices;
+
+namespace MysteryShopper.API.ImageControllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class CompanyImageController(ICompanyImageService companyImageService) : ControllerBase
+    {
+        [HttpGet("{companyId}")]
+        public async Task<FileStreamResult> GetAvatar(Guid companyId, CancellationToken cancellationToken)
+        {
+            var stream = await companyImageService.GetImageAsync(companyId, cancellationToken);
+
+            return File(stream, $"image/jpeg");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Company")]
+        public async Task UploadAvatar(IFormFile file, CancellationToken cancellationToken)
+        {
+            var currentCompanyId = HttpContext.GetIdFromContext();
+
+            await companyImageService.UploadImageAsync(currentCompanyId, file, cancellationToken);
+        }
+    }
+}
