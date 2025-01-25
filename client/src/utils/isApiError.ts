1a0c18d7432e4e4476be1acadc63f0e478f7e396
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2025-01-26 01:41:27 +0300

    Remove unused files

--- client/src/utils/isApiError.ts
+++
@@ -1,5 +0,0 @@
-import { ApiError } from "../api";
-
-export default function isApiError<T>(response: T | ApiError): response is ApiError {
-    return (response as ApiError).message !== undefined;
-}
